6b86dd4df6950f1653c10ac475de70c305b179f0
andres peña <andresesteb0772@gmail.com>
2025-04-03 11:45:19 -0500

    actualizacion main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from datos import productos
+from reactpy import componeent, html
+from reactpy.backend.Flask import configure
 
 app = Flask(__name__)
 app.run(host='0.0.0.0', debug=True)
